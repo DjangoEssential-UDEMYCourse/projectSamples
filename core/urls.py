f2e4d831ffa369386f59975f1af95aa2d73982ad
ALEXANDRE SAMPAIO <thndrmaster@gmail.com>
2021-11-05 16:15:55 -0300

    105. Precisa gerar PDF na sua aplicação Django? Vamos lá!

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import IndexView, DadosJsonView
+from .views import IndexView, DadosJsonView, RelatorioPDFView, Relatorio2View
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('charts/', IndexView.as_view(), name='charts'),
     path('dados/', DadosJsonView.as_view(), name='dados'),
+    path('relatorio/', RelatorioPDFView.as_view(), name='relatorio'),
+    path('relatorio2/', Relatorio2View.as_view(), name='relatorio-wp'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
-from random import randint
 
+from random import randint
 from django.views.generic import TemplateView
 from chartjs.views.lines import BaseLineChartView
+
+import io
+from django.http import FileResponse
+from django.views.generic import View
+from reportlab.pdfgen import canvas
+
+# *** do WeasyPrint
+from django.core.files.storage import FileSystemStorage
+from django.template.loader import render_to_string
+from django.http import HttpResponse
+from weasyprint import HTML
 
 
 class IndexView(TemplateView):
@@ -70,3 +81,48 @@ class DadosJsonView(BaseLineChartView):
         dados.append(dado)
 
     return dados
+
+# Relatorio PDF Utilizando o ReportLab
+class RelatorioPDFView(View):
+    def get(self, request, *args, **kwargs):
+        # Cria um arquivo para receber os dados e gerar um pdf
+        buffer = io.BytesIO()
+
+        # Cria o PDF
+        pdf = canvas.Canvas(buffer)
+
+        # Insere coisas no PDF
+        pdf.drawString(100, 100, 'Geek University')
+
+        #Quando acabamos de inserir coisas no PDF
+        pdf.showPage()
+        pdf.save()
+
+        # Por fim retornamos o buffer para o inicio do arquivo
+        buffer.seek(0)
+
+        # Faz o Download do Arquivo em PDF Gerado
+        # return FileResponse(buffer, as_attachment=True, filename='Relatorio.pdf')
+
+        # Abre o PDF diretamente no Navegador
+        return FileResponse(buffer, filename='Relatorio.pdf')
+
+
+class Relatorio2View(View):
+
+    def get(self, request, *args, **kwargs):
+        texto = ["Modelagem de Veiculos Automotivos",
+                 "Desenvolvimento de Software com metodologia ágil",
+                 "Desenvolvimento de Jogos com Unreal e C#"
+                ]
+        html_string = render_to_string('relatorio.html', {'texto': texto})
+        html = HTML(string=html_string)
+        html.write_pdf(target='/tmp/relatorio2.pdf')
+
+        fs = FileSystemStorage('/tmp')
+
+        with fs.open('relatorio2.pdf') as pdf:
+            response = HttpResponse(pdf, content_type='application/pdf')
+            # response['Content-Disposition'] = 'attachement; filename="relatorio2.pdf"'
+            response['Content-Disposition'] = 'inline; filename="relatorio2.pdf"'
+        return response
